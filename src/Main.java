89db98a74cbbd1f982a27762738ec3a31abce0fd
Aditya Kumar Singh <aditya345kumarsingh@gmail.com>
2026-07-26 18:25:45 +0530

    connection check

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,16 @@
 import java.sql.Connection;
-import java.sql.DriverManager;
 
 public class Main {
+
     public static void main(String[] args) {
 
-        String url = "jdbc:mysql://localhost:3306/college";
-        String user = "root";
-        String password = "MyNewPassword";
+        Connection con = DBConnection.getConnection();
 
-        try {
-            Connection con = DriverManager.getConnection(url, user, password);
-            System.out.println("Connected Successfully!");
-            con.close();
-        } catch (Exception e) {
-            System.out.println(e);
+        if (con != null) {
+            System.out.println("Database Connected Successfully!");
+        } else {
+            System.out.println("Database Connection Failed!");
         }
+
     }
 }
